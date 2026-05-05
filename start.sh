c290776c16e1d7831dc1e18f3905cee24555e2d4
Anujshintre <140949006+Anujshintre@users.noreply.github.com>
2026-05-05 22:28:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-# Download and run Tomcat with your WAR
+
+# Download Tomcat
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.52/bin/apache-tomcat-10.1.52.tar.gz
+
+# Extract
 tar -xzf apache-tomcat-10.1.52.tar.gz
-cp AjaraBazar.war apache-tomcat-10.1.52/webapps/ROOT.war
+
+# Copy your WAR file as ROOT (so app runs on base URL)
+cp Farmkart.war apache-tomcat-10.1.52/webapps/ROOT.war
+
+# Go to Tomcat bin directory
 cd apache-tomcat-10.1.52/bin
+
+# Start Tomcat
 ./catalina.sh run
