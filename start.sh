b9d7240a433668c25afb8f2cab24adb9f7acfb6a
Anujshintre <140949006+Anujshintre@users.noreply.github.com>
2026-05-07 22:32:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Download Tomcat
-wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.52/bin/apache-tomcat-10.1.52.tar.gz
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.105/bin/apache-tomcat-9.0.105.tar.gz
 
-# Extract
-tar -xzf apache-tomcat-10.1.52.tar.gz
+tar -xzf apache-tomcat-9.0.105.tar.gz
 
-# Copy your WAR file as ROOT (so app runs on base URL)
-cp Farmkart.war apache-tomcat-10.1.52/webapps/ROOT.war
+cp Farmkart.war apache-tomcat-9.0.105/webapps/ROOT.war
 
-# Go to Tomcat bin directory
-cd apache-tomcat-10.1.52/bin
+cd apache-tomcat-9.0.105/bin
 
-# Start Tomcat
+chmod +x catalina.sh
+
 ./catalina.sh run
